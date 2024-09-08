92ab64bd30207d00b159748fd334198c2552be75
tomohiro_0225 <express22212270@gmail.com>
2024-09-08 11:51:28 +0900

    1-20で実装したコメント機能を削除

--- thread/urls.py
+++ thread/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('<int:pk>/', views.TopicDetailView.as_view(), name='topic'),
 
     # 今回追加。シンプルなトピック投稿画面
-    path('create_topic/', views.TopAndCommentView.as_view(), name='create_topic'),
+    path('create_topic/', views.TopicCreateView.as_view(), name='create_topic'),
     
     # カテゴリーごと
     path('category/<str:url_code>/', views.CategoryView.as_view(), name='category'),
--- thread/views.py
+++ thread/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView, TemplateView, FormView, CreateView, ListView
 from django.urls import reverse_lazy
-from django import forms
 
-from . models import Topic, Category, Comment
-from . forms import TopicCreateForm ,TopicForm, CommentModelForm
+from . models import Topic, Category
+from . forms import TopicCreateForm ,TopicForm
 
 class TopicDetailView(DetailView):
     """トピック表示用クラス
@@ -50,27 +49,4 @@ class CategoryView(ListView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx['category'] = get_object_or_404(Category, url_code=self.kwargs['url_code'])
-        return ctx
-    
-# コメント
-class TopAndCommentView(FormView):
-    template_name = 'thread/detail_topic.html'
-    form_class = CommentModelForm
-    
-    def form_valid(self, form):
-        # comment = form.save(commit=False)
-        # comment.topic = Topic.ojbects.get(id=self.kwargs['pk'])
-        # comment.no = Comment.objects.filter(topic=self.kwargs['pk']).count() + 1
-        # comment.save()
-        
-        CommentModelForm.save_with_topic(self.kwargs.get('pk'))
-        return super().form_valid(form)
-    
-    def get_success_url(self):
-        return reverse_lazy('thread:topic', kwargs={'pk': self.kwargs['pk']})
-    
-    def get_context_data(self):
-        ctx = super().get_context_data()
-        ctx['topic'] = Topic.objects.get(id=self.kwargs['pk'])
-        ctx['comment_list'] = Comment.objects.filter(topic_id=self.kwargs['pk'].order_by('no'))
         return ctx
