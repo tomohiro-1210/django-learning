a7eb775bf88de2e96fc3c55d4c1bd99eadf94a64
tomohiro_0225 <express22212270@gmail.com>
2024-09-08 09:39:52 +0900

    1-7

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import views
+from django.views.generic import TemplateView
+from .views import *
 app_name = 'base'
 
 urlpatterns = [
-    path('', views.top, name='top'),
+    # path('', top, name='top'),
+    path('', TopView.as_view(), name='top'),
+    path('terms/', TemplateView.as_view(template_name='base/terms.html'), name='terms'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 def top(request):
     template = 'base/top.html'
     ctx = {'title': 'Django学習ちゃんねる(仮)'}
     return render(request, template, ctx)
+
+# TOPビュー
+
+class TopView(TemplateView):
+    template_name = 'base/top.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['title'] = 'IT学習ちゃんねる(仮)'
+        return ctx
