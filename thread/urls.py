54ff823a381751445eda501bcb3c86f0c204b49e
tomohiro_0225 <express22212270@gmail.com>
2024-09-08 10:02:01 +0900

    1-13 DetailView

--- thread/urls.py
+++ thread/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:pk>/', views.TopicDetailView.as_view(), name='topic'),
 ]
--- thread/views.py
+++ thread/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from . models import Topic
+
+class TopicDetailView(DetailView):
+    """トピック表示用クラス
+
+    DetailView使用例として作成
+
+    """
+    template_name = 'thread/detail_topic.html'
+    model = Topic
+    context_object_name = 'topic'
