6bcd5cad493df8b822921933ec194d953ffce1a3
tomohiro_0225 <express22212270@gmail.com>
2024-09-08 09:28:13 +0900

    1-4

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
+app_name = 'base'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.top, name='top'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def top(request):
+    template = 'base/top.html'
+    ctx = {'title': 'Django学習ちゃんねる(仮)'}
+    return render(request, template, ctx)
