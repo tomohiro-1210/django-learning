fa58865508aa1b7aa0f357c22457320069226452
tomohiro_0225 <express22212270@gmail.com>
2024-09-08 10:51:24 +0900

    1-18. 確認画面付きのトピック作成画面を作る

--- thread/urls.py
+++ thread/urls.py
@@ -4,12 +4,7 @@ from . import views
 urlpatterns = [
     # トピック詳細画面
     path('<int:pk>/', views.TopicDetailView.as_view(), name='topic'),
-    #path('<int:pk>/', views.TopicTemplateView.as_view(), name='topic'),
-    #path('<int:pk>/', views.detail_topic, name='topic'),
 
     # 今回追加。シンプルなトピック投稿画面
-    #path('create_topic/', views.simple_topic_create, name='create_topic'),
-    # path('create_topic/', views.topic_create, name='create_topic'),
-    # path('create_topic/', views.TopicFormView.as_view(), name='create_topic'),
     path('create_topic/', views.TopicCreateView.as_view(), name='create_topic')
 ]
--- thread/views.py
+++ thread/views.py
@@ -14,71 +14,7 @@ class TopicDetailView(DetailView):
     template_name = 'thread/detail_topic.html'
     model = Topic
     context_object_name = 'topic'
-    
-def simple_topic_create(request):
-    """シンプルなトピック投稿用関数
-
-    関数ベースの投稿機能作成例として実装
-
-    """
-    template_name = 'thread/create_topic.html'
-    ctx = {}
-    if request.method == 'GET':
-        ctx['form'] = TopicCreateForm()
-        return render(request, template_name, ctx)
-    
-    if request.method == 'POST':
-        topic_form = TopicCreateForm(request.POST)
-        if topic_form.is_valid():
-            topic_form.save()
-            return redirect(reverse_lazy('top'))
-        else:
-            ctx['form'] = topic_form
-            return render(request, template_name, ctx)
         
-def topic_create(request):
-    """トピック投稿用関数
-
-    関数ベースのFormを用いた投稿機能の実装例
-
-    """
-    template_name = 'thread/create_topic.html'
-    ctx = {}
-    if request.method == 'GET':
-        form = TopicForm()
-        ctx['form'] = form
-        return render(request, template_name, ctx)
-    
-    if request.method == 'POST':
-        topic_form = TopicForm(request.POST)
-        if topic_form.is_valid():
-            # topic_form.save()
-            topic = Topic()
-            cleaned_data = topic_form.cleaned_data
-            topic.title = cleaned_data['title']
-            topic.message = cleaned_data['message']
-            topic.user_name = cleaned_data['user_name']
-            topic.category = cleaned_data['category']
-            topic.save()            
-            return redirect(reverse_lazy('top'))
-        else:
-            ctx['form'] = topic_form
-            return render(request, template_name, ctx)
-        
-class TopicFormView(FormView):
-    """トピック投稿用関数
-
-    FormViewの練習用クラスとして作成
-
-    """
-    template_name = 'thread/create_topic.html'
-    form_class = TopicCreateForm
-    success_url = reverse_lazy('top')
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
-    
 
 class TopicCreateView(CreateView):
     """トピック投稿用関数
@@ -90,3 +26,15 @@ class TopicCreateView(CreateView):
     form_class = TopicCreateForm
     model = Topic
     success_url = reverse_lazy('top')
+    
+    def form_valid(self, form):
+        ctx = {'form': form}
+        if self.request.POST.get('next', '') == 'confirm':
+            return render(self.request, 'thread/confirm_topic.html', ctx)
+        if self.request.POST.get('next', '') == 'back':
+            return render(self.request, 'thread/create_topic.html', ctx)
+        if self.request.POST.get('next', '') == 'create':
+            return super().form_valid(form)
+        else:
+            # 正常動作ではここは通らない。エラーページへの遷移でも良い
+            return redirect(reverse_lazy('top'))
